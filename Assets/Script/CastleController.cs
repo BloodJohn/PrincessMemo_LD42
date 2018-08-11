bb7c9f5e990fb19d143af5dae4dfd8e21c27162f
Eugene Pavlov <bjohn.ext@gmail.com>
2018-08-11 12:04:42 +0300

    add Unity project

---
+++ Assets/Script/CastleController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CastleController : MonoBehaviour
+{
+    void Awake()
+    {
+        if (null == GameController.Instance) GameController.LoadScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void LoadScene()
+    {
+        Debug.LogFormat("CastleScene");
+        SceneManager.LoadSceneAsync("CastleScene");
+    }
+}
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+        UnityEngine.Random.InitState(DateTime.UtcNow.Millisecond);
+    }
+
+    private void Start()
+    {
+        CastleController.LoadScene();
+    }
+
+    public static void LoadScene()
+    {
+        Debug.LogFormat("LoadScene");
+        SceneManager.LoadSceneAsync("LoadScene");
+    }
+}
